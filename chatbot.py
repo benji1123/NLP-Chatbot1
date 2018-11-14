b5171016fd55c687d646ff91c0b0e0573e7f7624
Ben Li <benji112358@gmail.com>
2018-11-14 02:41:37 -0500

    preprocc

--- chatbot.py
+++ chatbot.py
@@ -8,26 +8,26 @@ import tensorflow as tf;
 import re;   #regex
 import time; #training time measurement
 
-
 #...................................... Part 1 - "Preprocessing" the Data ......................................
-# Datasets 
-lines=open("movie_lines.txt",encoding = 'utf-8',errors='ignore').read().split('\n'); # list: movie-lines (MLs) have unique ID#                  
-conversations=open("movie_conversations.txt",encoding='utf-8',errors='ignore').read().split('\n');  # (sets of ID#s [conversations]) form crucial training data 
 
-# Dictionary: ID-to-Line map 
+# Datasets 
+lines=open("movie_lines.txt",encoding = 'utf-8',errors='ignore').read().split('\n'); # list of movie-lines (MLs) w/ unique ID#                  
+conversations=open("movie_conversations.txt",encoding='utf-8',errors='ignore').read().split('\n');  # (sets of line-IDs [conversations]) form crucial training data 
+
+# Dictionary: mapping line-ID to Line  
 id2line = {}
 for l in lines:                             
     _line = l.split(' +++$+++ ')  
-    if len(_line)==5: # skip blank-lines
+    if len(_line)==5: # do not map blank-lines 
         id2line[_line[0]] = _line[4] 
 
-# List (Type List): Conversations
+# List (of Lists): Line-Groups/Conversations
 conversations_ids = []
 for conversation in conversations[:-1]: 
     _conv = conversation.split(" +++$+++ ")[-1][1:-1].replace("'","").replace(" ","") #noise-removal
     conversations_ids.append(_conv.split(','))
 
-# Mirror Qs & As as separate Lists
+# Lines are split between Qs & As (mirror lists)
 questions = []
 answers = []
 for conv in conversations_ids:
@@ -37,36 +37,25 @@ for conv in conversations_ids:
     if(len(questions) > len(answers)):
         answers.append("no comment")    # Preventing [Q's w/o A's] from de-aligning lists   
         
-# process text into its "vanilla form" 
+# Q&A-text is simplified to "vanilla"
 def clean(text):
     text = text.lower()
-    # removing contractions
-    text = re.sub(r"n't"," not", text)
-    text = re.sub(r"'re"," are", text)
-    text = re.sub(r"'m"," am", text)
-    text = re.sub(r"'ll"," will", text)
-    text = re.sub(r"'ve"," have", text)
-    text = re.sub(r"'d"," would", text)
-    # specifics
-    text = re.sub(r"i'm","i am", text)
-    text = re.sub(r"she's","she is", text)
-    text = re.sub(r"he's","he is", text)
-    text = re.sub(r"what's","what is", text)
-    text = re.sub(r"where's","where is", text)
-    text = re.sub(r"how's","how is", text)
-    text = re.sub(r"who's","who is", text)
-    text = re.sub(r"won't","will not", text)
+    # contractions are rm'd
+    text = re.sub(r"n't"," not", text);  			text = re.sub(r"'re"," are", text)
+    text = re.sub(r"'m"," am", text); 	 			text = re.sub(r"'ll"," will", text)
+    text = re.sub(r"'ve"," have", text); 			text = re.sub(r"'d"," would", text)
+    # families ↑ , specifics ↓ 
+    text = re.sub(r"i'm","i am", text);				text = re.sub(r"she's","she is", text)
+    text = re.sub(r"he's","he is", text);			text = re.sub(r"what's","what is", text)
+    text = re.sub(r"where's","where is", text);		text = re.sub(r"how's","how is", text)
+    text = re.sub(r"who's","who is", text);			text = re.sub(r"won't","will not", text)
     text = re.sub(r"can't","can not", text)
-    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]","",text) # manipulate this later
-    #text = re.sub(r"[~!@#$%^&*\"\'()-=_+{}[]|\:;\"\'`<>,?.]","", text)      
+    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]","",text) #abolished symbols list
     return text
+for thing in range(len(questions)): # text-formatting is applied
+    questions[thing],answers[thing] = clean(questions[thing]), clean(answers[thing])
 
-# apply text-formatting on all MLs
-for thing in range(len(questions)):
-    questions[thing] = clean(questions[thing])
-    answers[thing] = clean(answers[thing])
-
-# determine freq. of each word (in corpus) to eliminate trivial ones
+# freq. of words (in corpus) are computed
 word2freq = {}
 def count(dictionary, sentences):
     for sent in sentences:
@@ -75,9 +64,54 @@ def count(dictionary, sentences):
                 dictionary[word] = 1
             else:
                 dictionary[word] += 1
-
 count(word2freq, questions)
 count(word2freq, answers) 
-    
 
-    
+# Words are mapped to a unique-identifying integer (data compression)
+qWord2code, aWord2code = {}, {}       # the vocabulary for Q-interp & A-generation may vary 
+qThreshhold, aThreshhold = 20, 20     # decrease training-time by discarding in-frequent words 
+qWord_num, aWord_num = 0, 0 
+
+# non-frequent words are "forgotten" to speed-up training
+for word, freq in word2freq.items():
+    if(freq > qThreshhold):
+        qWord2code[word] = qWord_num
+        qWord_num += 1
+    if(freq > aThreshhold):
+        aWord2code[word] = aWord_num
+        aWord_num += 1
+
+tokens = ['<PAD>','EOS','OUT','SOS'] # special tokens in list
+for token in tokens:
+	qWord2code[token] = len(qWord2code) + 1
+	aWord2code[token] = len(aWord2code) + 1
+ 
+# Inverse mapping (form index to word) is facilitated 
+Anum2word = {index : word , for word, index in aWord2code.items()}
+# <EOS> is appended to each answer 
+for ans in range(len(answers)): answers[ans] += ' <EOS>'	# EOS token @ end of each answer
+
+# Q&As are simplified to contain equivalent-Integer-IDs instead of Words
+def compress_word_to_int(inputList):
+	outList = []
+	for sentence in inputList:
+		ints = []
+		for word in sentence.split(' '):
+			if word not in qWord2code:	# voided word
+				ints.append(qWord2code['<OUT>'])
+			else: ints.append(qWord2code[word])
+		outList.append(ints)
+	return outList
+questions_compressed = compress_word_to_int(questions)
+answers_compressed = compress_word_to_int(answers)
+
+
+# Sort Q's & A's by [length of question] to speeden training    ---------------  Test this!
+sorted_questions = []
+sorted_answers = []
+# Ignore null-qustions & questions w/ +24 words
+for question_length in range(1 : 25 + 1):
+    for question in enumerate(questions_compressed):
+        if(question[1] == l):
+            sorted_questions.append(questions_compressed[q[0]])
+            sorted_answers.append(answers_compressed[q[0]])
